e0cadc0742e7dd06d3de8cea3dee498cb00dafa9
Carlos Delgado (PC) casa <carlos16931@hotmail.com>
2020-04-06 15:16:07 -0500

    fin del curso

---
+++ src/views/Home.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <div class="flex justify-center">
+      <bounce-loader :loading="isLoading" :color="'#68d391'" :size="100" />
+    </div>
+    <px-assets-table v-if="!isLoading" :assets="assets"></px-assets-table>
+  </div>
+</template>
+
+<script>
+import api from "@/api.js";
+import PxAssetsTable from "@/components/PxAssetsTable";
+
+export default {
+  name: "Home",
+  components: {
+    PxAssetsTable
+  },
+  data() {
+    return {
+      assets: [],
+      isLoading: false
+    };
+  },
+  created() {
+    this.isLoading = true;
+    api.getAssets()
+    .then(assets => (this.assets = assets))
+    .finally(() => this.isLoading = false);
+  }
+};
+</script>
